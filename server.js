25c6cba5135caf375c464bc3d413733f902c6332
Aryanpatil1710 <151561766+Aryanpatil1710@users.noreply.github.com>
2025-05-17 16:40:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -56,11 +56,7 @@ app.use(helmet(``));
 app.use(xss());
 app.use(mongoSanitize());
 app.use(express.json());
-app.use(cors({
-  origin: '*', // or specify exact origin like 'https://editor.swagger.io'
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(morgan("dev"));
 
 //routes
